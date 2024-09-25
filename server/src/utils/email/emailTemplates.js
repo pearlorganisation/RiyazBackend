5c0527f8c9d53cf52cc32985155ee6a99f587554
nayan-kunwar26 <nayan@pearlorganisation.com>
2024-09-25 09:15:16 +0530

    minor fix

--- server/src/utils/email/emailTemplates.js
+++ server/src/utils/email/emailTemplates.js
@@ -2,7 +2,8 @@ import { sendMail } from "./sendMailService.js";
 
 export const sendForgotPasswordMail = async (email, resetToken) => {
   const subject = "Password reset request";
-  const resetLink = `http://localhost:3000/api/v1/user/reset-password/${resetToken}`; // later on change to ${process.env.CLIENT_URL}
+  //Following link should be the link of the frontend page responsible to request password reset. For now it is a direct endpoint to reset password
+  const resetLink = `http://localhost:3000/api/v1/user/reset-password/${resetToken}`; // later on change to ${process.env.FORGOT_PASSWORD_REDIRECT_URL}
   const templateName = "forgot-password-email";
   const templateData = { resetLink };
 
