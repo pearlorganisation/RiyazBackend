8dc2daa84742c2c854baf2ef9c6689ed440ff95e
nayan-kunwar26 <nayan@pearlorganisation.com>
2024-11-07 17:11:25 +0530

    comment

--- server/src/routes/vehicleRoutes.js
+++ server/src/routes/vehicleRoutes.js
@@ -1,21 +1,21 @@
-import express from "express";
-// import { verifyToken } from "../middleware/authMidleware.js";
-import { upload } from "../middleware/multer.js";
-import {
-  createVehicle,
-  getAllReviews,
-  getAllVehicles,
-  getSingleVehicle,
-} from "../controllers/vehicle/vehicleController.js";
+// import express from "express";
+// // import { verifyToken } from "../middleware/authMidleware.js";
+// import { upload } from "../middleware/multer.js";
+// import {
+//   createVehicle,
+//   getAllReviews,
+//   getAllVehicles,
+//   getSingleVehicle,
+// } from "../controllers/vehicle/vehicleController.js";
 
-const router = express.Router();
+// const router = express.Router();
 
-router
-  .route("/")
-  .post(upload.array("images", 5), createVehicle)
-  .get(getAllVehicles);
-// router.route("/").get(getAllVehicles);
-router.route("/:id").get(getSingleVehicle);
-router.route("/:id/reviews").get(getAllReviews);
+// router
+//   .route("/")
+//   .post(upload.array("images", 5), createVehicle)
+//   .get(getAllVehicles);
+// // router.route("/").get(getAllVehicles);
+// router.route("/:id").get(getSingleVehicle);
+// router.route("/:id/reviews").get(getAllReviews);
 
-export const vehicleRoutes = router;
+// export const vehicleRoutes = router;
