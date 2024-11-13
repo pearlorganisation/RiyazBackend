d96bdf8180feb168b3271d0c5e4be3d82eadb08b
Ranjan <ranjan@pearlorganisation.com>
2024-11-13 16:09:00 +0530

    added stripe payment

--- server/src/controllers/booking/bookingController.js
+++ server/src/controllers/booking/bookingController.js
@@ -1,17 +1,31 @@
 import { asyncHandler } from "../../utils/asyncHandler.js";
 import Stripe from "stripe"
+import dotenv from "dotenv";
+dotenv.config();
 
-const stripe = new Stripe(process.env.STRIPE_API_SECRET);
+const stripe = new Stripe(`${process.env.STRIPE_API_SECRET}`);
 
 export const createBooking = asyncHandler(async(req,res,next)=>{
    const {amount} = req.body;
-   try {
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount,
-        currency:"usd"
-    });
-    res.json({ clientSecret: paymentIntent.client_secret})
-   } catch (error) {
-    res.status(500).json({error: error.message})
-   }
+    console.log(amount, "amount received");
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+       line_items: [{
+           price_data: {
+               currency: "usd", // or the relevant currency
+               product_data: {
+                   name: "Booking Payment",
+               },
+               unit_amount: amount * 100, // Stripe expects amount in cents
+           },
+           quantity: 1,
+       }, 
+    ],
+        mode: "payment",
+        success_url: "http://localhost:5173/vehicle-details",
+        cancel_url: "http://localhost:5173/vehicles"
+    })
+    res.json({
+        id: session.id
+    })
 })
